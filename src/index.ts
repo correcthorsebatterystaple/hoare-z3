83c3e778f9cc080df2aa8bfecbee07d8ebba6be8
Burhanuddin Salim <burhan.salman@gmail.com>
2021-02-21 02:31:21 +0400

    Refactor from function class syntax

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import { getWeakestPrecondition } from "./preconditionEvaluator";
+import { readFileSync } from 'fs';
+import { getWeakestPrecondition, SourceInformation } from './preconditionEvaluator';
 
 const args = require('minimist')(process.argv.slice(2));
 const debug = args.debug || false;
 const fileName = args._[0];
 const toPrefix = args.prefix || false;
 
-const result = getWeakestPrecondition(fileName, {debug: debug, toPrefix: toPrefix});
+const sourceText = readFileSync(fileName, 'utf-8');
+const sourceInfo = new SourceInformation(fileName, sourceText);
 
-console.log(result);
+const weakestPrecondition = sourceInfo.getMainWeakestPrecondition();
+
+console.log(weakestPrecondition);
--- src/preconditionEvaluator.ts
+++ src/preconditionEvaluator.ts
@@ -3,102 +3,94 @@ import ts = require('typescript');
 import { assignmentTransform, conditionalTransform } from './hoareTransformers';
 import { infixToPrefix } from './infixToPrefix';
 
-export function getWeakestPrecondition(
-  fileName: string,
-  opts: { debug?: boolean; toPrefix?: boolean; sourceText?: string } = {}
-): string {
-  const debug = opts.debug || false;
-  const toPrefix = opts.toPrefix || false;
-  const sourceText = opts.sourceText || readFileSync(fileName, 'utf-8');
+export class SourceInformation {
+  private sourceFile: ts.SourceFile;
+  private main: ts.FunctionDeclaration;
+  public mainPrecondition: string;
+  public mainPostcondition: string;
 
-  const src = ts.createSourceFile(fileName, sourceText, ts.ScriptTarget.Latest);
-  const main = src.statements.filter((x) => ts.isFunctionDeclaration(x))[0] as ts.FunctionDeclaration;
+  constructor(private fileName: string, private sourceText: string) {
+    this.sourceFile = ts.createSourceFile(this.fileName, this.sourceText, ts.ScriptTarget.Latest);
+    this.main = this.sourceFile.statements.filter((x) => ts.isFunctionDeclaration(x))[0] as ts.FunctionDeclaration;
 
-  const rootPrecondition = getConditionFromNode(main, src);
+    this.mainPrecondition = getPreconditionFromNode(this.main, this.sourceFile);
 
-  const [postconditionRange] = ts.getTrailingCommentRanges(src.getFullText(), main.end);
-  const rootPostcondition = src
-    .getFullText()
-    .slice(postconditionRange.pos + 3, postconditionRange.end)
-    .trim();
-
-  const [weakestPrecondition] = getWeakestPreconditionFromNode(main.body, rootPostcondition, src, 0, debug);
-
-  return toPrefix ? infixToPrefix(weakestPrecondition) : weakestPrecondition;
-}
-
-export function getWeakestPreconditionFromNode(
-  node: ts.Node,
-  postcondition: string,
-  src: ts.SourceFile,
-  depth: number = 0,
-  debug = false
-): [precondition: string, text: string] {
-  if (debug) {
-    console.log('--'.repeat(depth), { node: node && node.getText(src), postcondition });
-  }
-  if (!node) return undefined;
-
-  // block statement
-  if (ts.isBlock(node)) {
-    if (node.statements.length > 0) {
-      // iterate through all the statements in the block and get their precondition successively
-      const precondition = node.statements.reduceRight((post, statement) => {
-        const newPre = getWeakestPreconditionFromNode(statement, post, src);
-        return newPre[0];
-      }, postcondition);
-
-      return [precondition, node.getText(src)];
-    } else {
-      return [postcondition, node.getText(src)];
-    }
+    const [postconditionRange] = ts.getTrailingCommentRanges(this.sourceFile.getFullText(), this.main.end);
+    this.mainPostcondition = this.sourceFile
+      .getFullText()
+      .slice(postconditionRange.pos + 3, postconditionRange.end)
+      .trim();
   }
 
-  // Assignment statement
-  if (
-    ts.isExpressionStatement(node) &&
-    ts.isBinaryExpression(node.expression) &&
-    node.expression.operatorToken.kind === ts.SyntaxKind.EqualsToken &&
-    ts.isIdentifier(node.expression.left)
-  ) {
-    // replaces all occurences of left side of assignment with right side of assignment in postcondition
-    const assignmentPrecondition = assignmentTransform(
-      postcondition,
-      node.expression.left.text,
-      node.expression.right.getText(src)
-    );
+  getMainWeakestPrecondition(
+    node: ts.Node = this.main.body,
+    postcondition: string = this.mainPostcondition,
+    opt?: { depth?: number; debug?: boolean }
+  ): string {
+    const depth = opt?.depth || 0;
+    const debug = opt?.debug || false;
 
     if (debug) {
-      console.log('--'.repeat(depth), assignmentPrecondition);
+      console.log('--'.repeat(depth), { node: node?.getText(this.sourceFile), postcondition });
     }
 
-    return [assignmentPrecondition, node.getText(src)];
-  }
+    if (!node) return undefined;
 
-  // If Statement
-  if (ts.isIfStatement(node) && ts.isBinaryExpression(node.expression)) {
-    let [thenPrecondition] = getWeakestPreconditionFromNode(node.thenStatement, postcondition, src, depth + 2);
-    let [elsePrecondition] = getWeakestPreconditionFromNode(node.elseStatement, postcondition, src, depth + 2) || [];
-
-    const expressionText = node.expression.getText(src);
-    const conditionalPrecondition = conditionalTransform(expressionText, thenPrecondition, elsePrecondition);
-
-    if (debug) {
-      console.log('--'.repeat(depth), conditionalPrecondition);
+    // block statement
+    if (ts.isBlock(node)) {
+      // iterate through all the statements  in reverse and derive the weakest precondition for the block
+      return node.statements.reduceRight(
+        (acc, statement) => this.getMainWeakestPrecondition(statement, acc, { depth: depth + 2 }),
+        postcondition
+      );
     }
 
-    return [conditionalPrecondition, node.getText(src)];
-  }
+    // Assignment statement
+    if (
+      ts.isExpressionStatement(node) &&
+      ts.isBinaryExpression(node.expression) &&
+      node.expression.operatorToken.kind === ts.SyntaxKind.EqualsToken &&
+      ts.isIdentifier(node.expression.left)
+    ) {
+      // replaces all occurences of left side of assignment with right side of assignment in postcondition
+      const assignmentPrecondition = assignmentTransform(
+        postcondition,
+        node.expression.left.text,
+        node.expression.right.getText(this.sourceFile)
+      );
 
-  // While statement
-  if (ts.isWhileStatement(node) && ts.isBinaryExpression(node.expression)) {
-    return [getConditionFromNode(node, src), node.getText(src)];
-  }
+      if (debug) {
+        console.log('--'.repeat(depth), assignmentPrecondition);
+      }
 
-  return undefined;
+      return assignmentPrecondition;
+    }
+
+    // If Statement
+    if (ts.isIfStatement(node) && ts.isBinaryExpression(node.expression)) {
+      let thenPrecondition = this.getMainWeakestPrecondition(node.thenStatement, postcondition, { depth: depth + 2 });
+      let elsePrecondition = this.getMainWeakestPrecondition(node.elseStatement, postcondition, { depth: depth + 2 });
+
+      const expressionText = node.expression.getText(this.sourceFile);
+      const conditionalPrecondition = conditionalTransform(expressionText, thenPrecondition, elsePrecondition);
+
+      if (debug) {
+        console.log('--'.repeat(depth), conditionalPrecondition);
+      }
+
+      return conditionalPrecondition;
+    }
+
+    // While statement
+    if (ts.isWhileStatement(node) && ts.isBinaryExpression(node.expression)) {
+      return getPreconditionFromNode(node, this.sourceFile);
+    }
+
+    throw new Error(`Node of type "${ts.SyntaxKind[node.kind]}" not implemented`);
+  }
 }
 
-export function getConditionFromNode(node: ts.Node, src: ts.SourceFile): string {
+export function getPreconditionFromNode(node: ts.Node, src: ts.SourceFile): string {
   const srcText = src.getFullText();
   const [firstCommentRange] = ts.getLeadingCommentRanges(srcText, node.getFullStart());
   const comment = srcText.slice(firstCommentRange.pos, firstCommentRange.end);
