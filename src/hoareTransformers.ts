3bf97ef39aa53394a9a086b12c8bf37428102f06
BSalim <burhan.salman@gmail.com>
2021-02-24 19:59:59 +0400

    Misc changes
    Add comments
    Add scripts
    change debug launch script

--- src/hoareTransformers.ts
+++ src/hoareTransformers.ts
@@ -3,6 +3,7 @@ import { tokenize } from './tokenizer';
 export function assignmentTransform(postcondition: string, left: string, right: string): string {
   const tokenizedPostcondition = tokenize(postcondition);
   return tokenizedPostcondition.reduce((acc, token) => {
+    // Replace all tokens equivalent to 'left' string with 'right' string
     if (token.type === 'id' && token.value === left) {
       return acc.concat('(', right, ')');
     }
