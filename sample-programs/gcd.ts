e663563c0d9926754cc4df933446c1231d1e7f53
BSalim <burhan.salman@gmail.com>
2021-04-08 19:32:08 +0400

    Add annotated files

--- sample-programs/gcd.ts
+++ sample-programs/gcd.ts
@@ -1,11 +1,12 @@
-//? a=x AND b=y AND a>0 AND b>0
+//? a=_X_ AND b=_Y_ AND a>0 AND b>0
 function gcd(a: number, b: number) {
-  //? 
-  while (a!=b) {
-    if (a>b) {
-      a = a-b;
+  //? gcd(a,b) = gcd(_X_,_Y_)
+  while (a != b) {
+    if (a > b) {
+      a = a - b;
     } else {
-      b=b-a;
+      b = b - a;
     }
   }
-}
+  return a;
+} //? $ret = gcd(_X_,_Y_)
